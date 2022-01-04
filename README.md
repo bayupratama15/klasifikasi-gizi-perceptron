5207192ea347a8d298085d8818ee569d359fb259
bayupratama15 <bolehjugaemail@gmail.com>
2022-01-04 16:45:03 +0700

    updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Klasifikasi Gizi Dengan Menggunakan Perceptron
+# Klasifikasi Gizi Dengan Menggunakan Metode Perceptron
 ```
 Nama    : Muhammad Bayu Pratama
 NIM     : 18.01.53.2082
--- index.php
+++ index.php
@@ -1,52 +1,68 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>PROJECT JST</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script type="text/javascript" src="js/jquery-3.2.0.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/jquery-3.2.0.min.js"></script>	
 </head>
+
 <body>
 	<nav class="navbar navbar-inverse navbar-fixed-top">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<a class="navbar-brand" href="index.php">Klasifikasi Gizi Mahasiswa</a>
-				</div>
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="index.php">Home</a></li>
-					</ul>
-				</div>
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">Klasifikasi Gizi Mahasiswa</a>
 			</div>
+		</div>
 	</nav>
 
 	<div class="container" style="margin-top: 50px;">
 		<div class="page-header">
 			<h1>Klasifikasi Gizi Mahasiswa Dengan Metode Perceptron</h1>
 		</div>
-				<div class="panel panel-primary">
-					<div class="panel-heading">
-						<h3>Menentukan Jumlah Data Training</h3>
+		<div class="panel panel-default">
+			<div class="panel-heading"><h3>Data Diri Pembuat Aplikasi</h3></div>
+			<div class="panel-body"  align="center">
+				<div class="row">
+					<div class="col-md-12">
+						<h4>Nama :</h4>
+						<h4>Muhammad Bayu Pratama</h4>
 					</div>
-					<div class="panel-body">
-						<form action="#" method="POST">
-							<div class="form-group">
-								<label for="sel1">Tambahkan Jumlah</label>
-									<select class="form-control" id="sel1" name="jmldata">
-									<?php 
-										for ($i=1; $i <= 100 ; $i++) { 
-											echo "<option>".$i."</option>";
-										}
-									 ?>								
-									</select>						
-							</div>
-								<input type="hidden" name="style" value="show">
-								<input type="submit" class="btn btn-primary" value="Submit" id="submit">
-						</form>	
+					<div class="col-md-12">
+						<h4>NIM :</h4>
+						<h4>18.01.53.2082</h4>
+					</div>
+					<div class="col-md-12">
+						<h4>MATKUL :</h4>
+						<h4>Jaringan Syaraf Tiruan</h4>
 					</div>
 				</div>
-				<?php
+			</div>
+		</div>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h3>Menentukan Jumlah Data Training</h3>
+			</div>
+			<div class="panel-body">
+				<form action="#" method="POST">
+					<div class="form-group">
+						<label for="sel1">Tambahkan Jumlah</label>
+						<select class="form-control" id="sel1" name="jmldata">
+							<?php 
+										for ($i=1; $i <= 2 ; $i++) { 
+											echo "<option>".$i."</option>";
+										}
+									 ?>
+						</select>
+					</div>
+					<input type="hidden" name="style" value="show">
+					<input type="submit" class="btn btn-primary" value="Submit" id="submit">
+				</form>
+			</div>
+		</div>
+		<?php
 					if(isset($_POST['style'])){
 						$style=$_POST['style'];
 						if(isset($_POST['jmldata'])){
@@ -59,23 +75,23 @@
 						$style='none';
 					}
 				?>
-				<div class="panel panel-primary" id="data" style="display: <?php echo $style; ?>">
-					<div class="panel-heading">
-						<h3>Data Sampel</h3>
-					</div>
-					<div class="panel-body" id="konten">
-						<form method="post" action="data.php">
-							<table class="table table-hover">
-								<thead>
-								<tr>
-									<th>Data ke</th>
-									<th>Tinggi Badan</th>
-									<th>Berat Badan</th>
-									<th>Aktivitas</th>
-								</tr>
-								</thead>
-								<tbody>
-									<?php
+		<div class="panel panel-primary" id="data" style="display: <?php echo $style; ?>">
+			<div class="panel-heading">
+				<h3>Data Sampel</h3>
+			</div>
+			<div class="panel-body" id="konten">
+				<form method="post" action="data.php">
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>Data ke</th>
+								<th>Tinggi Badan</th>
+								<th>Berat Badan</th>
+								<th>Aktivitas</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
 									for ($i=1; $i <= $x ; $i++) { 
 										echo "<tr>
 											  	<td>$i</td>
@@ -89,13 +105,14 @@
 											  </tr>";
 									}
 									?>
-								</tbody>
-							</table>
-							<input type="hidden" name="jmldata" value="<?php echo $x; ?>">
-							<button type="submit "class="btn btn-primary btn-block">Submit</button>
-						</form>
-					</div>
-				</div>
+						</tbody>
+					</table>
+					<input type="hidden" name="jmldata" value="<?php echo $x; ?>">
+					<button type="submit " class="btn btn-primary btn-block">Submit</button>
+				</form>
+			</div>
+		</div>
 	</div>
 </body>
+
 </html>
